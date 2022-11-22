3e0c0a8c304fbedd98c6d69942ca4b8553ac66cf
etrvic <etrvic@protonmail.com>
2022-11-22 19:00:11 +0200

    trecerea in adolescenta

--- utilities.php
+++ utilities.php
@@ -231,11 +231,13 @@ class Article
         $inRest = new Article("în rest, doar aparență", "", "28", "Aug", "inRest", "Anastasia Hangiu", "anastasiahagiu.jpg", "28/08/2022/10");
         $capatDeLinie = new Article("capăt de linie", "", "13", "Sep", "capatDeLinie", "Clara Pistol", "claraPistol.jpeg", "13/09/2022/14");
         $inNefiinta = new Article("În neființă", "", "13", "Sep", "inNefiinta", "Diana Georgescu", "dianaGeorgescu.jpeg", "13/09/2022/14");
+        $trecereaInAdolescenta = new Article("Trecere în adolescență", "", "22", "Nov", "trecereaInAdolescenta", "Ioana Busuioc", "ioanaBusuioc.jpeg", "22/11/2022/14");
 
         $articles = array(
 //            ADD HERE
 //            ADD HERE
 //            ADD HERE
+            $trecereaInAdolescenta,
             $inNefiinta,
             $capatDeLinie,
             $inRest,
@@ -382,9 +384,11 @@ class Article
         $suflet = new Article("Suflet din hârtie", "", "17", "Iun", "suflet", "Diana Georgescu", "unknown.jpeg", "17/06/2022/15");
         $capatDeLinie = new Article("capăt de linie", "", "13", "Sep", "capatDeLinie", "Clara Pistol", "claraPistol.jpeg", "13/09/2022/14");
         $inNefiinta = new Article("În neființă", "", "13", "Sep", "inNefiinta", "Diana Georgescu", "dianaGeorgescu.jpeg", "13/09/2022/14");
+        $trecereaInAdolescenta = new Article("Trecere în adolescență", "", "22", "Nov", "trecereaInAdolescenta", "Ioana Busuioc", "ioanaBusuioc.jpeg", "22/11/2022/14");
 
 
         $articles = array(
+            $trecereaInAdolescenta,
             $inNefiinta,
             $capatDeLinie,
             $suflet,
@@ -475,9 +479,11 @@ class Article
         $foiAlbe = new Article("În fața unei foi albe", "Un șir de bubuituri înfundate răsunară deasupra capului Norei. Înainte de a se obișnui cu ticurile neobișnuite ale noii sale vecine, firava bătrânică mai că făcuse un atac de panică prima oară când aceste sunete răzbătuseră prin tavanul ce îi separa apartamentul de cel al Xeniei, fata zăpăcită care se mutase nu demult deasupra ei.", "22", "Apr", "foiAlbe", "Ioana Busuioc", "unknown.jpeg", "22/04/2022/18");
         $trixie = new Article("Trixie (1/2)", "Mereu m-am întrebat de ce copiii trebuie să învețe istoria unor civilizații care au dispărut acum mult timp. Se spune că școala evidențiază și arată vocația elevilor. Dar când vine vorba de istorie, ce legătură are aceasta cu aptitudinile?", "10", "Mai", "trixie", "Călin Mihai Tănase", "unknown.jpeg", "10/05/2022/18");
         $primavara = new Article("Primăvara: nostalgie sau renaștere?", "Imaginați-vă o inimă. O inimă rănită, pansată cu trandafiri. Trandafirii au țepi care adâncesc rănile din centrul trupului, dar și petale, care le ocrotesc. Cum poate ceva atât de delicat și de frumos să cauzeze atâtea răni și suferință ? Cum poate ceva ce te-a rănit să te și vindece ?", "31", "Mai", "primavara", "Ana Negoiță", "unknown.jpeg", "31/05/2022/17");
+        $trecereaInAdolescenta = new Article("Trecere în adolescență", "", "22", "Nov", "trecereaInAdolescenta", "Ioana Busuioc", "ioanaBusuioc.jpeg", "22/11/2022/14");
 
 
         $articles = array(
+            $trecereaInAdolescenta,
             $primavara,
             $trixie,
             $foiAlbe,
